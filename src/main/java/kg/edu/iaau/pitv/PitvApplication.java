fbb9c4dd2db046ddb6a1f89e196cf8b81c349336
Unknown <ali.ubisoft@gmail.com>
2018-01-14 13:58:50 +0600

    initialize spring boot project

---
+++ src/main/java/kg/edu/iaau/pitv/PitvApplication.java
@@ -0,0 +1,12 @@
+package kg.edu.iaau.pitv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitvApplication.class, args);
+	}
+}
---
+++ src/test/java/kg/edu/iaau/pitv/PitvApplicationTests.java
@@ -0,0 +1,16 @@
+package kg.edu.iaau.pitv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PitvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
